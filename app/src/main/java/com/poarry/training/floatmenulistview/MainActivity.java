60b14966e7d38f642ed833c33cd70157a99ba7dd
PoarryScript <719151128@qq.com>
2016-02-18 16:23:17 +0800

    replace findViewById by ButterKnife

--- app/src/main/java/com/poarry/training/floatmenulistview/MainActivity.java
+++ app/src/main/java/com/poarry/training/floatmenulistview/MainActivity.java
@@ -7,17 +7,21 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.poarry.training.floatmenulistview.adapter.ListViewAdapter;
 import com.poarry.training.floatmenulistview.view.SuspendedTopListView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private SuspendedTopListView mListview;
+    @Bind(R.id.list_view)
+    SuspendedTopListView mListview;
     private View mFloatMenuView;
 
     private Button firstMenu;
@@ -30,8 +34,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mListview = (SuspendedTopListView) findViewById(R.id.list_view);
+
         View mHeadviews = LayoutInflater.from(getApplicationContext()).inflate(R.layout.viewpager_layout, null);
         mFloatMenuView = mHeadviews.findViewById(R.id.fixheader);
 
@@ -49,30 +54,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mViewPager.setOnClickListener(this);
 
         initToolBar();
-    }
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -115,6 +96,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         .setAction("Action", null).show();
             }
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
